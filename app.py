ee1ba7ab5b6988a1feae2a21ee8cb32ff9e24ac6
Ben Tettmar <83777519+bentettmar@users.noreply.github.com>
2022-03-01 20:51:50 +0000

    turn debug off in app.py
    made app.py run in production mode when double clicked.

--- app.py
+++ app.py
@@ -14,4 +14,4 @@ for route_file in os.listdir("routes"):
         app.register_blueprint(lib.blueprint)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=False, host='0.0.0.0', port=80)
